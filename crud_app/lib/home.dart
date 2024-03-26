d7abfe915355d56ef2bc890433c6996729cde77b
Sagar Kumbhar <sagarkumbhar2018@gmail.com>
2024-03-26 16:33:37 +0530

    ui done

--- crud_app/lib/home.dart
+++ crud_app/lib/home.dart
@@ -12,7 +12,75 @@ class _HomeScreen extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
-        title: const Text("data"),
+        title: const Text(
+          "Crud App",
+          style: TextStyle(
+            color: Color.fromRGBO(255, 255, 255, 1),
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "Enter Your Name",
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "Enter Your Mobile No",
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                        Color.fromARGB(255, 206, 188, 236)),
+                  ),
+                  onPressed: () {},
+                  child: const Text("Creat"),
+                ),
+                ElevatedButton(
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                        Color.fromARGB(255, 206, 188, 236)),
+                  ),
+                  onPressed: () {},
+                  child: const Text("Read"),
+                ),
+                ElevatedButton(
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                        Color.fromARGB(255, 206, 188, 236)),
+                  ),
+                  onPressed: () {},
+                  child: const Text("Update"),
+                ),
+                ElevatedButton(
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(
+                        Color.fromARGB(255, 206, 188, 236)),
+                  ),
+                  onPressed: () {},
+                  child: const Text("Delete"),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
